71925a135783f75d9feb608afdaaa399ebcec782
Abigail Berger <aberger2019@pingry.org>
2017-05-01 12:43:10 -0400

    Test file

---
+++ caesarTest.py
@@ -0,0 +1,6 @@
+import caesarCipher
+
+print(caesarCipher.caesar_cipher("We're in comp sci right now.", 3))
+print(caesarCipher.caesar_cipher("hello", 450))
+print(caesarCipher.caesar_cipher("This is a test!!", 10))
+print(caesarCipher.caesar_cipher("We're in comp sci right now.", 0))
